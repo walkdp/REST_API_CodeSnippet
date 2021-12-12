f84b61d9758acf334340aa66ce6b75b86e49c578
walkdp <95473271+walkdp@users.noreply.github.com>
2021-12-12 10:45:17 +0545

    Version-one

---
+++ React-Redux.js
@@ -0,0 +1,66 @@
+The folder that contain action creater: For crud operation and searching.
+Similarly, the reducer: which updates the application state crosspnding to dispatch action.
+
+What are the module or dependecy need for CRUD operations Using React and Redux.
+-react-redux
+-redux
+-redux-thunk
+-rect-router-dom
+-axios
+-bootstrap
+
+folder structure:
+src
+-actions
+--tutoral.js
+--types.js
+
+-components
+--AddTutorial
+--TutorialList.js
+--Tutorial.js
+
+
+-reducer
+--index.js
+--tutorals.js
+
+-services
+--TutorialService.js: Has method for sending HTTP request to the Apis.
+
+//To handle any request.
+
+App.js
+http-common.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
