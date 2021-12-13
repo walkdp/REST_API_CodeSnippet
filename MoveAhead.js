debd044309ee3b5ea9dbb9fae03da0990c67a79b
walkdp <95473271+walkdp@users.noreply.github.com>
2021-12-13 14:05:50 +0545

    Version first

---
+++ MoveAhead.js
@@ -0,0 +1,69 @@
+DP you have to know how to deal with data in React and Redux.
+You have to know how do we create actions,reducer and services.
+
+Actions:
+CRUD operations and types.
+Actions: 
+Action Types:
+export const SET_FORM_DATA = 'SET_FORM_DATA';
+export const CLEAR_FORM_DATA = 'CLEAR_FORM_DATA';
+export const SET_SUCCESS_FORM_SUBMIT = 'SET_SUCCESS_FORM_SUBMIT';
+export const CLEAR_SUCCESS_STATE = 'CLEAR_SUCCESS_STATE';
+
+
+Steps to solve the problem right.
+See how the other data is fetched..
+As well as make clear use of redux make it try..lets....
+
+
+What my mind think....
+If your are not become capable of it yet....
+
+Way: 01
+You mind wants domination or negative and thinks about success to show them how powerful you are...
+
+Way: 02
+As positive perceptive as I will have this, that people will know me how powerful i am, think about interviews after success.
+Make more proud to yourself. I am this and I am that...
+Mind always thinks about how to get achievement fast...
+What are the steps to get things faster...
+Mind doesnot wants to get busy on actions....
+
+Be aware and watch the nature of your mind. How actually it works.
+
+My be the options gets things not done in time, if you are dependent upon other you will kill your own strength and get things done by other.
+
+Watch your mind....
+Do things make little bit progrssive.
+
+buildingPermitAction.js
+create action......
+
+
+//React and Redux.
+In class based components we are using componentDidMount, componentsDidUpdate, and componentWillMount.
+Function hooks: {useEffect, useDispatch, useSelector}
+In functional based component we are using useEffect hooks which is all the combination of componentDidMount, componentDidUpdate and componentWillMount.
+
+useDispatch: sends our new state of task from our api and to our Store.
+
+Acessing data in Redux...
+The various option to fetch data redux-thunk and redux-sage..
+
+Right way to learn the things: Do not see the videos and do copy their way of code.
+
+Select the single project and do, you start to do......
+While doing you have to know nothing about that just do and awake how the things are going. Do focus on that.
+
+
+
+
+
+
+
+
+
+
+
+
+
